cda53fe9884c9e131f622f6018dc3d1358070511
BIT Student <classroom@bgit.rs>
2018-03-16 15:49:53 +0100

    Reactintro

---
+++ firstReact (1)/app.js
@@ -0,0 +1,89 @@
+// const root = document.getElementById('root');
+
+// const Shop = (props) => {
+//     return (
+//         <li>{props.items}</li>
+//     )
+// }
+
+// const ShopList = (props) => {
+//     return props.items.map(e => <Shop items = {e}/>)
+// }
+
+// const App = (props) => {
+//     return (
+//         <div>
+//         <h1>sfdg</h1>
+//         <ShopList items = {props.name}/>
+//         </div>
+//     )   
+// }
+
+
+
+// ReactDOM.render(<App name = {["da","dafs","asdf"]} />, root);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const root = document.getElementById("root")
+
+const ShopList = (props) => {
+    return (
+        props.items.map(x => {x})
+    )
+}
+
+
+const App = (props) => {
+    return (
+        <div>
+        <h1>{props.title}</h1>
+            <ShopList items={props.name}/>
+
+        </div> 
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render (<App title={"Title"} name={["alalalaalal","lalalaalalal","lolololollo"]}/>,root)
